b761e750aa29c4bf34abc218240b382f40f9ab12
Richa Joshi <richajoshi@richas-mbp-2.home>
2025-11-12 22:05:14 +0100

    feat: added user interface

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var { graphql, buildSchema } = require("graphql")
+var express = require("express");
+var { createHandler } =  require("graphql-http/lib/use/express");
+var {ruruHTML} = require("ruru/server")
 
 var schema = buildSchema(`
   type Query {
@@ -9,8 +12,20 @@ var schema = buildSchema(`
 
 var rootValue = { hello: () => "Hello world!", age: () => 20 }
 
-var source = "{ hello, age }"
+/*var source = "{ hello, age }"
 
 graphql({ schema, source, rootValue }).then(response => {
     console.log(response)
+})*/
+
+const app = express();
+
+app.all("/graphql", createHandler({ schema, rootValue }));
+
+app.get("/", (req, res) => {
+    res.type("html");
+    res.end(ruruHTML({endpoint: "/graphql"}))
 })
+
+app.listen(4000);
+console.log("Server running on localhost:4000");
